7f012c55b48185ecee5ec84b3e4621af823f573b
Enkilol <87070269+Enkilol@users.noreply.github.com>
2023-03-24 00:07:19 +0100

    Update FVpicker.js

--- FVpicker.js
+++ FVpicker.js
@@ -129,9 +129,9 @@ function messaggio() {
 const frutto = messaggio();
 
 function shareOnTwitter(frutto) {
-  const tweetUrl = "https://twitter.com/intent/tweet?&text=";
+  const tweetUrl = "https://twitter.com/intent/tweet?url=&text=";
   const tweetText = `Healyhy picker mi ha consigliato di mangiare: ${frutto.nome}. Il prodotto contiene queste vitamine: ${frutto.vitamine} #frutta #verdura #salute #food #healthyfood #${frutto.nome}`;
-  const mediaUrl = "https://media1.giphy.com/media/gPqBCc8xZuW4xApjqB/giphy.gif?cid=ecf05e47mwb79fjpp8l2utdj4z5pvyutzirak44eg7jiq8vm&rid=giphy.gif&ct=g";
+  const mediaUrl = "";
   const url = tweetUrl + encodeURIComponent(tweetText) + "&media=" + encodeURIComponent(mediaUrl);
   window.open(url);
 }
